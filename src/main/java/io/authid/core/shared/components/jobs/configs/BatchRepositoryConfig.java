807e21e4fb4f4f4ff2966d667a2adf0014ec91f8
elhakimdev99 <abdulelhakim68@gmail.com>
2025-08-22 01:38:27 +0700

    fix: setup new CI CD for github action use node js semver

--- src/main/java/io/authid/core/shared/components/jobs/configs/BatchRepositoryConfig.java
+++ src/main/java/io/authid/core/shared/components/jobs/configs/BatchRepositoryConfig.java
@@ -2,13 +2,8 @@ package io.authid.core.shared.components.jobs.configs;
 
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
